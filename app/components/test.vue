7dd1b296cecb79cdea7290ba26c9c043b756a853
huyuangang <1730790894@qq.com>
2018-03-08 00:08:00 +0800

    测试页

---
+++ app/components/test.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div class="cc">
+        {{text}}
+        <p class="title">aaaa</p>
+    </div>
+</template> 
+
+<script>
+export default {
+  data() {
+      return {
+          text: 'jajajja'
+      }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+    .cc {
+        color: red;
+        .title {
+            color: green;
+        }
+    }
+</style>
+
---
+++ app/entrys/login.js
@@ -0,0 +1,8 @@
+
+import Vue from 'vue'
+import test from '../components/test.vue'
+
+new Vue({
+    el: '#app',
+    render: h => h(test)
+})
